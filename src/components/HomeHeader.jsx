cf7cad4581dede24d29785eef3c642acd4cc3d3f
Juan Manuel Velez <jumavxp@hotmail.com>
2025-12-11 08:14:11 -0500

    Cleaned base code, styles and pictures. Added Home page and components folder

---
+++ src/components/HomeHeader.jsx
@@ -0,0 +1,13 @@
+function HomeHeader(){
+    return(
+        <nav>
+            <div>
+                Bienes Raices Sena
+            </div>
+            <div>
+                Crear Cuenta | Iniciar sesion
+            </div>
+        </nav>
+    )
+}
+export default HomeHeader;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import HomeHeader from "../components/HomeHeader"
+
+function HomePage(){
+    return (
+    <>
+    <HomeHeader/>
+    <h1>Hola Mundo</h1>
+    </>
+)
+}
+export default HomePage
